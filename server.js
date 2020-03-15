409a8f58a6f41212c53f38fa97ffb613c64d6f0b
Beatriz Januário <bibi_januario@hotmail.com>
2020-03-14 23:26:16 -0300

    continuação do backend

--- server.js
+++ server.js
@@ -4,12 +4,47 @@ const nunjucks = require('nunjucks') //permite usar variaveis, funcoes, etc no h
 
 server.use(express.static('public'))
 
+server.use(express.urlencoded({ extended: true })) //habilita o body do formulário
+
 nunjucks.configure('./', {
-    express: server
+    express: server,
+    noCache: true //força a atualização do array donors ao invés de armazená-lo em cache
 })
 
+const donors = [
+    {
+        name: "Beatriz Januário",
+        blood: "AB+"
+    },
+    {
+        name: "Maria Silva",
+        blood: "B+"
+    },
+    {
+        name: "João Santos",
+        blood: "A+"
+    },
+    {
+        name: "Carla Oliveira",
+        blood: "O+"
+    }
+]
+
 server.get('/', function(req, res) {
-    return res.render('index.html')
+    return res.render('index.html', { donors })
+})
+
+server.post('/', function(req, res) {
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+
+    donors.push({
+        name: name,
+        blood: blood
+    })
+
+    return res.redirect('/')
 })
 
 server.listen(3000)
